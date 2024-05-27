f8a2fc02707617c33a8a772ba300f491ab1ece7c
Mbithe <mbithemwangi@gmail.com>
2024-05-27 16:55:17 -0700

    add all files

--- __test__/main.test.js
+++ __test__/main.test.js
@@ -1,7 +1,14 @@
 const {fizzBuzz} =require('../main.js');
-describe('fizzBuzz function tests', ()=>{
+
  test ("should return FizzBuzz for numbers divisible by both 3 and 5",() => {
-    expect(fizzBuzz(45)).toBe("FizzBuzz")
+    expect(45 % 5 && 15 % 3).toBe(0)
  })   
-}
-)
+ test ("should return Fizz for numbers divisible by 3",() => {
+    expect( 9 % 3).toBe(0)
+ })   
+ test ("should return FizzBuzz for numbers divisible by  5",() => {
+    expect(10 % 5 ).toBe(0)
+ })   
+ test ("should return i for numbers not divisible by both 3 and 5",() => {
+    expect(11 % 5 && 4 % 3).toBe(1)
+ })   
--- main.js
+++ main.js
@@ -1,25 +1,24 @@
 // Function to print FizzBuzz
-function fizzBuzz() {
-  for (let i = 1; i <= 100; i++) {
+function fizzBuzz(i) {
+  
       // Check and verify whether the number is both divisible by 3 and 5
       if (i % 3 === 0 && i % 5 === 0) {
-          console.log("FizzBuzz");
+          return  "FizzBuzz";
       }
       // Verify if the number is divisible by 3
       else if (i % 3 === 0) {
-          console.log("Fizz");
+          return  "Fizz";
       }
       // Check if the number is divisible by 5
       else if (i % 5 === 0) {
-          console.log("Buzz");
+          return "Buzz";
       }
       // Otherwise, just print the number
       else {
-          console.log(i);
+          return i;
       }
   }
-}
+
 
 // Call the function to print FizzBuzz
-fizzBuzz();
-module.exports={fizzBuzz};
+console.log(fizzBuzz(45));
